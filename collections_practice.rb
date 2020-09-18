af16484d4b35957d8d543ca1c7e95718811c611e
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-18 04:02:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,10 +46,9 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |word, index|
+  array.collect do |word, index|
     if  word != array[1]
       word << "s"
     end
-    array
   end
 end
